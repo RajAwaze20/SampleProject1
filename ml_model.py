4d44e36a7be5dd4a8554aa25ba4779b9890e0826
RajAwaze20 <rajendra.awaze@gmail.com>
2026-07-16 20:09:45 +1000

    Added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -2,3 +2,13 @@
 
 print('adding code related to model')
 
+def model1():
+    print('Adding code to model1')
+
+def model2():
+    print('Adding code to model2')
+
+def model3():
+    print('Adding code to model3')
+
+
